fb1bfccb498fb4d46ae6b2a072a6aa6b41e3edc1
Kip Hampton <khampton@totalcinema.com>
2011-07-02 08:15:03 -0700

    More experimental hoohah with Bread::Board

--- temp/lib/MyApp/Resource/One.pm
+++ temp/lib/MyApp/Resource/One.pm
@@ -3,13 +3,19 @@ use Moose;
 extends 'Magpie::Resource';
 use Magpie::Constants;
 use Data::Dumper::Concise;
-use Plack::App::File;
+
+has simple_arg => (
+        is => 'rw',
+        isa => 'Str',
+        required => 1,
+        default => 'Teh Schnoper',
+);
 
 sub GET {
     my $self = shift;
     my $ctxt = shift;
     my $p = __PACKAGE__;
-    warn "$p GET called\n";
+    warn "$p GET called. simple arg is " . $self->simple_arg . "\n";
 }
 
 
