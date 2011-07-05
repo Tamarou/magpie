36c70b981fd3062e35c98442bfc481cc0a12b18b
Kip Hampton <khampton@totalcinema.com>
2011-07-05 01:05:55 -0700

    Compartmentailize breadboard config.

--- lib/Magpie/Breadboard.pm
+++ lib/Magpie/Breadboard.pm
@@ -1,25 +1,50 @@
 package Magpie::Breadboard;
 use Moose;
 use Bread::Board;
-
+use Bread::Board::Dumper;
 extends 'Bread::Board::Container';
 
 has '+name' => ( default => 'Application' );
 
-has resources => (
-    is          =>  'rw',
-    isa         =>  'Bread::Board::Container',
-    lazy_build  => 1,
-    #init_arg    => 'Resources',
-);
-
-sub _build_resources {
-    return Bread::Board::Container->new( name => 'Resources' );
-}
-
 sub BUILD {
     my $self = shift;
-    container $self => as { container 'Resources' => as{ $self->resources }};
+    container $self => as {
+        container 'Assets' => as {};
+        container 'MagpieInternal' => as {};
+    };
+}
+
+sub assets {
+    my $self = shift;
+    if ( my $new_container = shift ) {
+        delete $self->sub_containers->{'Assets'};
+        $new_container->name('Assets');
+        $self->add_sub_container( $new_container );
+    }
+    return $self->get_sub_container('Assets');
+}
+
+sub add_asset {
+    my $self = shift;
+    my $thing = shift;
+    warn "WTF?????";
+    my $assets = $self->get_sub_container('Assets');
+
+    warn "internal " . Bread::Board::Dumper->new->dump( $assets );
+    if ( $thing->isa('Bread::Board::Container') ||
+         $thing->isa('Bread::Board::Container::Parameterized') ) {
+        $assets->add_sub_container( $thing );
+    }
+    elsif ( $thing->isa('Bread::Board::Service') ) {
+        $assets->add_service( $thing );
+    }
+    else {
+        confess "add_asset requires a type => typmap pair, service, or container."
+    }
+}
+
+sub resolve_asset {
+    return shift->assets->resolve( @_ );
 }
 
 1;
--- t/breadboard.t
+++ t/breadboard.t
@@ -3,19 +3,25 @@ use FindBin;
 use lib "$FindBin::Bin/lib";
 use Data::Dumper::Concise;
 use Bread::Board;
+use Bread::Board::Dumper;
+
 use_ok('Magpie::Machine');
 
-my $resources = container 'Resources' => as {
+my $resources = container 'Wibble' => as {
         service 'somevar' => 'some value';
 };
 
+my $foo = service 'othervar' => 'other value';
+
 my $m = Magpie::Machine->new();
 
-$m->breadboard->resources( $resources );
+$m->breadboard->assets( $resources );
 
 ok( $m );
 
-warn Dumper( $m );
+$m->breadboard->add_asset( $foo );
+
+warn "\n" . Bread::Board::Dumper->new->dump( $m->breadboard );
 
 $m->pipeline(qw( Magpie::Pipeline::Breadboard::Simple ));
 
