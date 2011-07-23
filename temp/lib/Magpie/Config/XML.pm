a2bfccaee43536dfe627fc5cb5762243172248fd
Kip Hampton <khampton@totalcinema.com>
2011-07-23 16:07:25 -0700

    experimental XML config loader inproves.

--- temp/lib/Magpie/Config/XML.pm
+++ temp/lib/Magpie/Config/XML.pm
@@ -11,23 +11,56 @@ sub process {
     my $root = $dom->documentElement;
 
     foreach my $pipe ($root->findnodes('//pipeline')) {
-        foreach my $kid ($pipe->childNodes) {
-            my $kid_name = $kid->localname;
-            next unless $kid_name; # skip txt nodes here
-            if ( $kid_name eq 'add' ) {
-
+        foreach my $pipe_child ($pipe->childNodes) {
+            my $pipe_child_name = $pipe_child->localname;
+            next unless $pipe_child_name; # skip txt nodes here
+            if ( $pipe_child_name eq 'add' ) {
+                process_add( $pipe_child );
             }
-            elsif ( $kid_name eq 'match' ) {
-            warn Dumper( $kid );
+            elsif ( $pipe_child_name eq 'match' ) {
+                warn Dumper( $pipe_child );
+            }
+            else {
+                warn "Unknown child element '$pipe_child_name' in config.\n";
+            }
         }
     }
-
-
 
     return @stack;
 }
 
 sub process_add {
+    my $node = shift;
+    my $class_name = $node->findvalue('@class|./class/text()');
+    my $params = {};
+    if ($node->exists('./parameters')) {
+        foreach my $param ($node->findnodes('./parameters/*')) {
+            warn Dumper( $param );
+            my ($name, $value) = (undef, undef);
+            if ($param->localname eq 'parameter' ) {
+                $name = $param->findvalue('@name|./name/text()');
+                $value = $param->findvalue('@value|./value/text()');
+            }
+            else {
+                $name = $param->localname;
+                $value = $param->findvalue('./text()');
+                $params->{$name} = $value;
+            }
+
+            $value =~ s/^\s+//;
+	        $value =~ s/\s+$//;
+
+            if( $name && $value ) {
+                $params->{$name} = $value;
+            }
+        }
+    }
+
+    return ($class_name, $params);
+}
+
+sub trim_whitespace {
+    my $string = shift;
 
 }
 
